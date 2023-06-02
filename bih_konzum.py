65936d65962e3464e31d015f819f701ea1afd37b
Zoran Kradija <zoran.kradija@hotmail.com>
2023-06-02 18:41:28 +0200

    dodan bih konzum

---
+++ bih_konzum.py
@@ -0,0 +1,94 @@
+import requests
+import time
+from datetime import date
+import pyodbc as odbc
+import sys
+import config
+
+kat = [ ['Kandit', 'https://www.konzumshop.ba/v2/categories/5471244/products?filter%5Bsubcategory_ids%5D%5B%5D=5471252&filter%5Bshow%5D=all&filter%5Bsort%5D=nameAsc&filter%5Bsort_field%5D=name&page=1&per_page=1000&time=1685723560399'],
+        ['Kandit', 'https://www.konzumshop.ba/v2/categories/5471244/products?filter%5Bsubcategory_ids%5D%5B%5D=5471257&filter%5Bshow%5D=all&filter%5Bsort%5D=nameAsc&filter%5Bsort_field%5D=name&page=1&per_page=1000&time=1685723765060'],
+        ['Kandit', 'https://www.konzumshop.ba/v2/categories/5471244/products?filter%5Bsubcategory_ids%5D%5B%5D=5471265&filter%5Bshow%5D=all&filter%5Bsort%5D=nameAsc&filter%5Bsort_field%5D=name&page=1&per_page=1000&time=1685723789845'],
+        ['Saponia', 'https://www.konzumshop.ba/v2/categories/5472087/products?filter%5Bshow%5D=all&filter%5Bsort_field%5D=soldStatistics&filter%5Bsort%5D=soldStatistics&page=1&per_page=1000&time=1685723815904'],
+        ['Saponia', 'https://www.konzumshop.ba/v2/categories/5471607/products?filter%5Bshow%5D=all&filter%5Bsort_field%5D=soldStatistics&filter%5Bsort%5D=soldStatistics&page=1&per_page=1000&time=1685723857930'],
+        ]
+
+def main():
+    # identificiram se kao Chrome browser
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "Accept-Encoding": "*",
+        "Connection": "keep-alive",
+    }
+
+    result = []
+
+    web_mjesto=11
+    trgovina = 21
+    datum = str(date.today())
+
+    pocetak_vrijeme = time.time()
+    
+    s = requests.Session()
+
+    for k in kat:
+        r = s.request('GET', k[1], headers=headers)
+        data = r.json()
+
+        for d in data['products']:
+            product = []
+            product.append(web_mjesto)
+            product.append(trgovina)
+            product.append(datum)
+            product.append('https://www.konzumshop.ba' + d['product_path'])
+            product.append(k[0])
+            product.append(d['code'])
+            product.append(d['name'])
+            product.append(float(d['price']['amount'])/100)
+            product.append(d['barcodes'][0])
+            result.append(product)
+            print(product)
+        time.sleep(1)
+
+    # insert u SQL bazu    
+    server = config.server
+    database = config.database
+    username = config.username
+    password = config.password
+
+    conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    conn = odbc.connect(conn_str)
+    cursor = conn.cursor()
+
+    try:
+        conn = odbc.connect(conn_str)
+    except Exception as e:
+        print(e)
+        print('Task is terminated')
+    else:
+        cursor = conn.cursor()
+
+    insert_statement = '''
+        insert into cijene (WebMjestoId,TrgovinaId,datum,poveznica,kategorija,sifra,naziv,cijena,GtinKom) 
+        values (?,?,?,?,?,?,?,?,?)
+        '''
+
+    try:
+        for r in result:
+            cursor.execute(insert_statement, r)
+    except Exception as e:
+        cursor.rollback()
+        print(e.value)
+        print('Transaction rolled back')
+    else:
+        print(f'{len(result)} records inserted successfully')
+        cursor.commit()
+        cursor.close()
+  
+
+    kraj_vrijeme = time.time()
+    ukupno_vrijeme = kraj_vrijeme - pocetak_vrijeme
+    print(ukupno_vrijeme)
+
+
+if __name__ == "__main__":
+    main()
---
+++ crna_gora_voli_trade.py
@@ -0,0 +1,114 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from datetime import date
+import pyodbc as odbc
+import sys
+import config
+
+kat =   [['Kandit','https://voli.me/kategorije/52'],
+        ['Kandit','https://voli.me/kategorije/53'],
+        ['Kandit','https://voli.me/kategorije/54'],
+        ['Kandit','https://voli.me/kategorije/59'],
+        ['Saponia','https://voli.me/kategorije/188'],
+        ['Saponia','https://voli.me/kategorije/189'],
+        ['Saponia','https://voli.me/kategorije/213'],
+        ['Saponia','https://voli.me/kategorije/214'],
+        ['Saponia','https://voli.me/kategorije/216'],
+        ['Saponia','https://voli.me/kategorije/217'],
+        ['Saponia','https://voli.me/kategorije/218'],
+        ['Saponia','https://voli.me/kategorije/220'],
+        ['Saponia','https://voli.me/kategorije/222'],
+        ['Saponia','https://voli.me/kategorije/223'],
+        ['Saponia','https://voli.me/kategorije/224']]
+
+
+
+
+def main():
+    # identificiram se kao Chrome browser
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "Accept-Encoding": "*",
+        "Connection": "keep-alive",
+    }
+
+    pocetak_vrijeme = time.time()
+    s = requests.Session()
+
+    result = []
+    web_mjesto=10
+    trgovina=20
+    datum = str(date.today())
+    pocetak_vrijeme = time.time()
+
+    product_dict = {}
+    for k in kat:
+        print(k)
+        response = s.get(k[1], headers=headers)
+        web_page = response.text
+        soup = BeautifulSoup(web_page, 'html.parser')
+
+        for div in soup.find_all('div', {'class': 'card-body p-3'}):
+            if str(div.find('div', {'class': 'card-text'}).find('a')['href']).split('/')[-1] in product_dict:
+                break
+            product = []
+            product.append(web_mjesto)
+            product.append(trgovina)
+            product.append(datum)
+            product.append(str(div.find('div', {'class': 'card-text'}).find('a')['href']))
+            product.append(k[0])
+            product.append(str(div.find('div', {'class': 'card-text'}).find('a')['href']).split('/')[-1])
+            product.append(div.find('div', {'class': 'card-text'}).get('title').strip())
+            if div.find('div',{'class':'product-price pl-special text-primary'}):
+                product.append(float(div.find('div',{'class': 'price-wrapper'}).find('div',{'class': 'product-price pl-special text-primary'}).get_text().strip().split('€')[0].replace(',','')))
+            else: 
+                product.append(float(div.find('div',{'class': 'price-wrapper'}).find('div',{'class': 'product-price text-secondary'}).get_text().strip().split('€')[0].replace(',','')))
+            product_dict[product[5]]=1
+
+            result.append(product)
+        time.sleep(1)
+
+    # insert u SQL bazu    
+    server = config.server
+    database = config.database
+    username = config.username
+    password = config.password
+
+    conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    conn = odbc.connect(conn_str)
+    cursor = conn.cursor()
+
+    try:
+        conn = odbc.connect(conn_str)
+    except Exception as e:
+        print(e)
+        print('Task is terminated')
+        sys.exit
+    else:
+        cursor = conn.cursor()
+
+    insert_statement = '''
+        insert into cijene (WebMjestoId,TrgovinaId,datum,poveznica,kategorija,sifra,naziv,cijena) 
+        values (?,?,?,?,?,?,?,?)
+        '''
+
+    try:
+        for r in result:
+            cursor.execute(insert_statement, r)
+    except Exception as e:
+        cursor.rollback()
+        print(e.value)
+        print('Transaction rolled back')
+    else:
+        print(f'{len(result)} records inserted successfully')
+        cursor.commit()
+        cursor.close()
+
+    kraj_vrijeme = time.time()
+    ukupno_vrijeme = kraj_vrijeme - pocetak_vrijeme
+    print(ukupno_vrijeme)
+
+
+if __name__ == "__main__":
+    main()        
