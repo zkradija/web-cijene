9ca43ce53539eaed0b41656f2663aefef019ae1f
Zoran Kradija <zoran.kradija@hotmail.com>
2023-06-07 15:11:27 +0200

    added kosovo_super_viva

---
+++ kosovo_super_viva.py
@@ -0,0 +1,70 @@
+import json
+import time
+from datetime import date, datetime
+
+from fake_headers import fake_headers
+from insert_sql import insert_sql
+
+
+kat =   [['Kandit','https://spv-online-api.tframe.team/customer/products/',
+            '4', '12'],
+        ['Kandit', 'https://spv-online-api.tframe.team/customer/products/',
+            '4', '15'],
+        ['Kandit', 'https://spv-online-api.tframe.team/customer/products/',
+            '4', '14'],
+        ['Saponia', 'https://spv-online-api.tframe.team/customer/products/',
+            '2', '34'],
+        ['Saponia', 'https://spv-online-api.tframe.team/customer/products/',
+            '2', '30'],
+        ['Saponia', 'https://spv-online-api.tframe.team/customer/products/',
+            '2', '36']]
+
+# url = ?search&category=4&subcategory=12&page=1&sort=description
+
+def main():
+    print(f'{__file__} : {datetime.now().strftime("%H:%M:%S")}')
+    result=[]
+    indProxy = 0
+    web_site = 18
+    trgovina = 28
+    date_str = str(date.today())
+
+    # there is no barcode so im using dummy data
+    barcode = ''    
+    start_time = time.time()
+    for k in kat:
+        print(k)
+        url_page_number = f'https://spv-online-api.tframe.team/customer/products/?search&category={k[2]}&subcategory={k[3]}&page=1&sort=description'
+        r = fake_headers(url_page_number, indProxy)
+        data = json.loads(r)
+        page_number = int(data['total_pages'])
+
+        for x in range (1,page_number+1):
+            url = f'https://spv-online-api.tframe.team/customer/products/?search&category={k[2]}&subcategory={k[3]}&page={x}&sort=description'
+            r = fake_headers(url, indProxy)
+            data = json.loads(r)
+
+            for d in data['results']:
+                product=[]
+                product.append(web_site)
+                product.append(trgovina)
+                product.append(date_str)
+                product.append('https://www.super-viva.com/produkt/' + d['product_id'])
+                product.append(k[0])
+                product.append(d['product_id'])
+                product.append(d['description'])
+                product.append(float(d['price']))
+                product.append(barcode)
+                result.append(product)
+                
+            time.sleep(1)
+    
+    # inserting data
+    insert_sql(result)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f'Elapsed time: {int(elapsed_time)} seconds')
+
+if __name__ == "__main__":
+    main()
--- srbija_maxi.py
+++ srbija_maxi.py
@@ -31,7 +31,7 @@ def main():
         data = json.loads(r)
         page_number = int(data['data']['categoryProductSearch']['pagination']['totalPages'])
 
-        for x in range (0,page_number):
+        for x in range (0,page_number + 1):
             url = 'https://www.maxi.rs/api/v1/?operationName=GetCategoryProductSearch&variables=%7B%22lang%22%3A%22sr%22%2C%22searchQuery%22%3A%22%3Arelevance%22%2C%22sort%22%3A%22relevance%22%2C%22category%22%3A%22' + str(k[2]) + '%22%2C%22pageNumber%22%3A' + str(x) + "%2C%22pageSize%22%3A20%2C%22filterFlag%22%3Atrue%2C%22plainChildCategories%22%3Afalse%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2265b0b1aacb2caeea6468873e8f7fde0ef82ffb0c4f9c93583c41070fa1f13c82%22%7D%7D"
             r = fake_headers(url, indProxy)
             data = json.loads(r)
@@ -45,9 +45,13 @@ def main():
                 product.append(k[0])
                 product.append(d['code'])
                 product.append(d['name'])
-                product.append(float(d['price']['unitPrice']))
+                product.append(float(d['price']['discountedPriceFormatted']
+                    .replace(' RSD','')
+                    .replace('.','')
+                    .replace(',','.')))
                 product.append(barcode)
                 result.append(product)
+                print(product)
                 
             time.sleep(1)
     
